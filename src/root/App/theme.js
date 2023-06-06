50ec4323924b50d52db2412632dc8f0d3434d345
Angelika Krygier <angelika.krygier96@gmail.com>
2023-06-06 13:51:17 +0200

    prepared application to work

---
+++ src/root/App/theme.js
@@ -0,0 +1,26 @@
+const common = {
+  breakpoint: {
+    mobileMax: 767,
+  },
+  fontWeight: {
+    regular: 400,
+    medium: 500,
+    bold: 700,
+    hardBold: 900,
+  },
+};
+
+const colorName = {
+  black: "#ccccc",
+  white: "#fffff",
+};
+
+export const light = {
+  ...common,
+  colors: {},
+};
+
+export const dark = {
+  ...common,
+  colors: {},
+};
