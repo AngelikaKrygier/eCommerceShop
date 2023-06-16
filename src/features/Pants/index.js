cd00c36bc5ef17124f03c6415ca5fb1a347fb5ff
Angelika Krygier <angelika.krygier96@gmail.com>
2023-06-16 22:36:18 +0200

    added Pants component

---
+++ src/features/Pants/index.js
@@ -0,0 +1,56 @@
+import { Content } from "../../common/Content";
+
+export const Pants = () => {
+  const pants = [
+    {
+      name: "Nazwa wzoru 1",
+      id: "1a",
+      imagePath: "https://i.postimg.cc/76x53tJ1/PERSONAL-EXPENSE-TRACKER-3.png",
+      description:
+        "Spodnie wykonane z wysokiej jakości materiałów, zapewniają wygodę i doskonałe dopasowanie",
+      prise: "80,00 zł",
+    },
+    {
+      name: "Nazwa wzoru 2",
+      id: "1b",
+      imagePath: "https://i.postimg.cc/76x53tJ1/PERSONAL-EXPENSE-TRACKER-3.png",
+      description:
+        "Spodnie wykonane z wysokiej jakości materiałów, zapewniają wygodę i doskonałe dopasowanie",
+      prise: "80,00 zł",
+    },
+    {
+      name: "Nazwa wzoru 3",
+      id: "1c",
+      imagePath: "https://i.postimg.cc/76x53tJ1/PERSONAL-EXPENSE-TRACKER-3.png",
+      description:
+        "Spodnie wykonane z wysokiej jakości materiałów, zapewniają wygodę i doskonałe dopasowanie",
+      prise: "80,00 zł",
+    },
+    {
+      name: "Nazwa wzoru 4",
+      id: "1d",
+      imagePath: "https://i.postimg.cc/76x53tJ1/PERSONAL-EXPENSE-TRACKER-3.png",
+      description:
+        "Spodnie wykonane z wysokiej jakości materiałów, zapewniają wygodę i doskonałe dopasowanie",
+      prise: "80,00 zł",
+    },
+    {
+      name: "Nazwa wzoru 5",
+      id: "1e",
+      imagePath: "https://i.postimg.cc/76x53tJ1/PERSONAL-EXPENSE-TRACKER-3.png",
+      description:
+        "Spodnie wykonane z wysokiej jakości materiałów, zapewniają wygodę i doskonałe dopasowanie",
+      prise: "80,00 zł",
+    },
+    {
+      name: "Nazwa wzoru 6",
+      id: "1f",
+      imagePath: "https://i.postimg.cc/76x53tJ1/PERSONAL-EXPENSE-TRACKER-3.png",
+      description:
+        "Spodnie wykonane z wysokiej jakości materiałów, zapewniają wygodę i doskonałe dopasowanie",
+      prise: "80,00 zł",
+    },
+  ];
+
+  return <Content title="Spodnie" list={pants} />;
+};
