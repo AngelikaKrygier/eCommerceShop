9500ee437f1c4546c3502de1115d9f76b8e1188d
Angelika Krygier <angelika.krygier96@gmail.com>
2023-06-16 22:37:02 +0200

    added Dresses component

---
+++ src/features/Dresses/index.js
@@ -0,0 +1,56 @@
+import { Content } from "../../common/Content";
+
+export const Dresses = () => {
+  const dresses = [
+    {
+      name: "Nazwa wzoru 1",
+      id: "1",
+      imagePath: "https://i.postimg.cc/1X3n0VFW/PERSONAL-EXPENSE-TRACKER-2.png",
+      description:
+        "Sukienka Wykonana z wysokiej jakości materiałów, zapewnia wygodę i doskonałe dopasowanie",
+      prise: "100,00 zł",
+    },
+    {
+      name: "Nazwa wzoru 2",
+      id: "1",
+      imagePath: "https://i.postimg.cc/1X3n0VFW/PERSONAL-EXPENSE-TRACKER-2.png",
+      description:
+        "Sukienka Wykonana z wysokiej jakości materiałów, zapewnia wygodę i doskonałe dopasowanie",
+      prise: "100,00 zł",
+    },
+    {
+      name: "Nazwa wzoru 3",
+      id: "1",
+      imagePath: "https://i.postimg.cc/1X3n0VFW/PERSONAL-EXPENSE-TRACKER-2.png",
+      description:
+        "Sukienka Wykonana z wysokiej jakości materiałów, zapewnia wygodę i doskonałe dopasowanie",
+      prise: "100,00 zł",
+    },
+    {
+      name: "Nazwa wzoru 4",
+      id: "1",
+      imagePath: "https://i.postimg.cc/1X3n0VFW/PERSONAL-EXPENSE-TRACKER-2.png",
+      description:
+        "Sukienka Wykonana z wysokiej jakości materiałów, zapewnia wygodę i doskonałe dopasowanie",
+      prise: "100,00 zł",
+    },
+    {
+      name: "Nazwa wzoru 5",
+      id: "1",
+      imagePath: "https://i.postimg.cc/1X3n0VFW/PERSONAL-EXPENSE-TRACKER-2.png",
+      description:
+        "Sukienka Wykonana z wysokiej jakości materiałów, zapewnia wygodę i doskonałe dopasowanie",
+      prise: "100,00 zł",
+    },
+    {
+      name: "Nazwa wzoru 6",
+      id: "1",
+      imagePath: "https://i.postimg.cc/1X3n0VFW/PERSONAL-EXPENSE-TRACKER-2.png",
+      description:
+        "Sukienka Wykonana z wysokiej jakości materiałów, zapewnia wygodę i doskonałe dopasowanie",
+      prise: "100,00 zł",
+    },
+  ];
+
+  return <Content title="Sukienki" list={dresses} />;
+};
