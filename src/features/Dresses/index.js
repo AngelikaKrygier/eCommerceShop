dbd6d281cef5321d5f196a6ee7697bd149769c30
Angelika Krygier <angelika.krygier96@gmail.com>
2023-06-19 18:35:45 +0200

    connected state's elemnts with component's propses (Blouses, Dresses, Pants, Shoes components)

--- src/features/Dresses/index.js
+++ src/features/Dresses/index.js
@@ -1,56 +1,5 @@
 import { Content } from "../../common/Content";
 
-export const Dresses = () => {
-  const dresses = [
-    {
-      name: "Nazwa wzoru 1",
-      id: "1",
-      imagePath: "https://i.postimg.cc/1X3n0VFW/PERSONAL-EXPENSE-TRACKER-2.png",
-      description:
-        "Sukienka Wykonana z wysokiej jakości materiałów, zapewnia wygodę i doskonałe dopasowanie",
-      prise: "100,00 zł",
-    },
-    {
-      name: "Nazwa wzoru 2",
-      id: "1",
-      imagePath: "https://i.postimg.cc/1X3n0VFW/PERSONAL-EXPENSE-TRACKER-2.png",
-      description:
-        "Sukienka Wykonana z wysokiej jakości materiałów, zapewnia wygodę i doskonałe dopasowanie",
-      prise: "100,00 zł",
-    },
-    {
-      name: "Nazwa wzoru 3",
-      id: "1",
-      imagePath: "https://i.postimg.cc/1X3n0VFW/PERSONAL-EXPENSE-TRACKER-2.png",
-      description:
-        "Sukienka Wykonana z wysokiej jakości materiałów, zapewnia wygodę i doskonałe dopasowanie",
-      prise: "100,00 zł",
-    },
-    {
-      name: "Nazwa wzoru 4",
-      id: "1",
-      imagePath: "https://i.postimg.cc/1X3n0VFW/PERSONAL-EXPENSE-TRACKER-2.png",
-      description:
-        "Sukienka Wykonana z wysokiej jakości materiałów, zapewnia wygodę i doskonałe dopasowanie",
-      prise: "100,00 zł",
-    },
-    {
-      name: "Nazwa wzoru 5",
-      id: "1",
-      imagePath: "https://i.postimg.cc/1X3n0VFW/PERSONAL-EXPENSE-TRACKER-2.png",
-      description:
-        "Sukienka Wykonana z wysokiej jakości materiałów, zapewnia wygodę i doskonałe dopasowanie",
-      prise: "100,00 zł",
-    },
-    {
-      name: "Nazwa wzoru 6",
-      id: "1",
-      imagePath: "https://i.postimg.cc/1X3n0VFW/PERSONAL-EXPENSE-TRACKER-2.png",
-      description:
-        "Sukienka Wykonana z wysokiej jakości materiałów, zapewnia wygodę i doskonałe dopasowanie",
-      prise: "100,00 zł",
-    },
-  ];
-
-  return <Content title="Sukienki" list={dresses} />;
-};
+export const Dresses = ({ state, products }) => (
+  <Content state={state} title={"Sukienki"} list={products.dresses} />
+);
--- src/features/Shoes/index.js
+++ src/features/Shoes/index.js
@@ -1,56 +1,5 @@
 import { Content } from "../../common/Content";
 
-export const Shoes = () => {
-  const shoes = [
-    {
-      name: "Nazwa wzoru 1",
-      id: "1aa",
-      imagePath: "https://i.postimg.cc/C11qy6Mh/PERSONAL-EXPENSE-TRACKER-4.png",
-      description:
-        "Buty wykonane z wysokiej jakości materiałów, zapewniają wygodę i doskonałe dopasowanie",
-      prise: "120,00 zł",
-    },
-    {
-      name: "Nazwa wzoru 2",
-      id: "1bb",
-      imagePath: "https://i.postimg.cc/C11qy6Mh/PERSONAL-EXPENSE-TRACKER-4.png",
-      description:
-        "Buty wykonane z wysokiej jakości materiałów, zapewniają wygodę i doskonałe dopasowanie",
-      prise: "120,00 zł",
-    },
-    {
-      name: "Nazwa wzoru 3",
-      id: "1cc",
-      imagePath: "https://i.postimg.cc/C11qy6Mh/PERSONAL-EXPENSE-TRACKER-4.png",
-      description:
-        "Buty wykonane z wysokiej jakości materiałów, zapewniają wygodę i doskonałe dopasowanie",
-      prise: "120,00 zł",
-    },
-    {
-      name: "Nazwa wzoru 4",
-      id: "1dd",
-      imagePath: "https://i.postimg.cc/C11qy6Mh/PERSONAL-EXPENSE-TRACKER-4.png",
-      description:
-        "Buty wykonane z wysokiej jakości materiałów, zapewniają wygodę i doskonałe dopasowanie",
-      prise: "120,00 zł",
-    },
-    {
-      name: "Nazwa wzoru 5",
-      id: "1ee",
-      imagePath: "https://i.postimg.cc/C11qy6Mh/PERSONAL-EXPENSE-TRACKER-4.png",
-      description:
-        "Buty wykonane z wysokiej jakości materiałów, zapewniają wygodę i doskonałe dopasowanie",
-      prise: "120,00 zł",
-    },
-    {
-      name: "Nazwa wzoru 6",
-      id: "1ff",
-      imagePath: "https://i.postimg.cc/C11qy6Mh/PERSONAL-EXPENSE-TRACKER-4.png",
-      description:
-        "Buty wykonane z wysokiej jakości materiałów, zapewniają wygodę i doskonałe dopasowanie",
-      prise: "120,00 zł",
-    },
-  ];
-
-  return <Content title="Buty" list={shoes} />;
-};
+export const Shoes = ({ state, products }) => (
+  <Content state={state} title={"Buty"} list={products.shoes} />
+);
