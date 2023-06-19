dabbd740f20a7d649139b1f10504a7ea93ce7066
Angelika Krygier <angelika.krygier96@gmail.com>
2023-06-19 18:37:20 +0200

    conected store with application

--- src/index.js
+++ src/index.js
@@ -5,13 +5,17 @@ import reportWebVitals from "./reportWebVitals";
 import { ThemeProvider } from "styled-components";
 import { light } from "./root/App/theme";
 import { GlobalStyle } from "./root/App/GlobalStyle";
+import { Provider } from "react-redux";
+import { store } from "./root/App/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={light}>
       <GlobalStyle />
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ThemeProvider>
   </React.StrictMode>
 );
