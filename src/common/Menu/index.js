c6d7d817d6bee5ad150181c0ce180e98d767bef4
Angelika Krygier <angelika.krygier96@gmail.com>
2023-06-19 18:36:13 +0200

    updated Menu component

--- src/common/Menu/index.js
+++ src/common/Menu/index.js
@@ -2,7 +2,7 @@ import { MenuItem, StyledMenu } from "./styled";
 
 export const Menu = () => (
   <StyledMenu>
-    <MenuItem>Blog</MenuItem>
+    <MenuItem>Strona Główna</MenuItem>
     <MenuItem>Sukienki</MenuItem>
     <MenuItem>Spodnie</MenuItem>
     <MenuItem>Topy</MenuItem>
