5a2971e854fbb9595140a5f914ee0f1653dcfe34
Angelika Krygier <angelika.krygier96@gmail.com>
2023-06-21 13:47:40 +0200

    added second banner to BackDrop

--- src/common/BackDrop/index.js
+++ src/common/BackDrop/index.js
@@ -1,7 +1,17 @@
-import { StyledBackDrop } from "./styled";
+import { useState } from "react";
+import {
+  StyledBackDrop,
+  StyledVectorPrevious,
+  StyledVextorNext,
+} from "./styled";
 
-export const BackDrop = () => (
-    <StyledBackDrop>
-        
+export const BackDrop = () => {
+  const [backDrop, setBackDrop] = useState(false);
+
+  return (
+    <StyledBackDrop second={backDrop}>
+      <StyledVectorPrevious onClick={() => setBackDrop(!backDrop)} />
+      <StyledVextorNext onClick={() => setBackDrop(!backDrop)} />
     </StyledBackDrop>
-)
+  );
+};
--- src/common/BackDrop/styled.js
+++ src/common/BackDrop/styled.js
@@ -1,5 +1,8 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import backDrop from "../../images/backDrop.png";
+import backDropSecond from "../../images/BackDrop2.png";
+import { ReactComponent as VectorPrevious } from "../../images/vectorPrevious.svg";
+import { ReactComponent as VectorNext } from "../../images/vectorNext.svg";
 
 export const StyledBackDrop = styled.div`
   background-image: url(${backDrop});
@@ -8,8 +11,35 @@ export const StyledBackDrop = styled.div`
   aspect-ratio: 4/1;
   background-size: cover;
   box-shadow: ${({ theme }) => theme.boxShadow};
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  transition: 0.8s;
+
+  ${({ second }) =>
+    second &&
+    css`
+      background-image: url(${backDropSecond});
+    `}
 
   @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
     display: none;
   }
+`;
+
+export const StyledVextorNext = styled(VectorNext)`
+  width: 50px;
+  height: 50px;
+  align-self: center;
+  justify-self: end;
+  cursor: pointer;
+  padding-right: 20px;
+`;
+
+export const StyledVectorPrevious = styled(VectorPrevious)`
+  width: 50px;
+  height: 50px;
+  align-self: center;
+  justify-self: start;
+  cursor: pointer;
+  padding-left: 20px;
 `;
