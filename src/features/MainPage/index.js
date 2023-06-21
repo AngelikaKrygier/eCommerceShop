7eef57f83388a1dc160d1123d184a00bacea254f
Angelika Krygier <angelika.krygier96@gmail.com>
2023-06-21 14:10:46 +0200

    added introductory content to MainPage

--- src/features/MainPage/index.js
+++ src/features/MainPage/index.js
@@ -1,5 +1,10 @@
+import { Container, Image } from "./styled";
+import firstImage from "../../images/Bestsellery.png";
+import secondImage from "../../images/nowosci.png";
 
 export const MainPage = () => (
-  <></>
+  <Container>
+    <Image src={firstImage} />
+    <Image additionalStyles src={secondImage} />
+  </Container>
 );
- 
--- src/features/MainPage/styled.js
+++ src/features/MainPage/styled.js
@@ -1,1 +1,27 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+    grid-template-columns: 1fr;
+    align-content: center;
+  }
+`;
+
+export const Image = styled.img`
+  max-width: 600px;
+  width: 100%;
+
+  ${({ additionalStyles }) =>
+    additionalStyles &&
+    css`
+      margin-top: 90px;
+    `}
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+    justify-self: center;
+    margin: 10px;
+  }
+`;
