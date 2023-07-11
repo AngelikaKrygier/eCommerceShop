dfeb716d42909ae1da4642a2afc91bc67be23c76
Angelika Krygier <angelika.krygier96@gmail.com>
2023-07-11 19:52:45 +0200

    used generic component UserWindow to create content of components: CreateAccount and Login.

--- src/features/user/CreateAccount/index.js
+++ src/features/user/CreateAccount/index.js
@@ -1,1 +1,17 @@
-export const CreateAccount = () => <></>;
+import { UserWindow } from "../../../common/UserWindow";
+import { toShopRules } from "../../../root/routes";
+
+export const CreateAccount = () => (
+  <UserWindow
+    userName={"Nazwa Użytkownika"}
+    password={"Hasło"}
+    button={"Załóż Konto"}
+    paragraph={
+      "Zakładając konto akceptujesz regulamin sklepu. Koniecznie zapoznaj się z "
+    }
+    nameLink={"regulaminem sklepu"}
+    navLink={toShopRules}
+    email={"e-mail"}
+    createAccount
+  />
+);
--- src/features/user/Login/index.js
+++ src/features/user/Login/index.js
@@ -1,27 +1,15 @@
-import {
-  Button,
-  Input,
-  InputName,
-  Paragraph,
-  UserLoginForm,
-  StyledNavLink,
-} from "./styled";
 import { toCreateAccount } from "../../../root/routes";
+import { UserWindow } from "../../../common/UserWindow";
 
 export const Login = () => {
   return (
-    <UserLoginForm>
-      <InputName>Login:</InputName>
-      <Input></Input>
-      <InputName>Hasło:</InputName>
-      <Input></Input>
-      <Button>Zaloguj</Button>
-      <Paragraph>
-        Nie masz konta?{" "}
-        <StyledNavLink to={toCreateAccount}>
-            Załóż Konto!
-        </StyledNavLink>
-      </Paragraph>
-    </UserLoginForm>
+    <UserWindow
+      userName={"Login"}
+      password={"Hasło"}
+      button={"Zaloguj"}
+      paragraph={"Nie masz konta? "}
+      nameLink={"Załóż konto"}
+      navLink={toCreateAccount}
+    />
   );
 };
