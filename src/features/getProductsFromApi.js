c16ecd4988b9203996fcea336b358374ff6e705e
Angelika Krygier <angelika.krygier96@gmail.com>
2023-07-17 20:37:14 +0200

    added connecting with firebasedatabase

--- src/features/getProductsFromApi.js
+++ src/features/getProductsFromApi.js
@@ -1,5 +1,5 @@
 export const getProductsFromApi = async () => {
-  const response = await fetch("/eCommerceShop/products.json");
+  const response = await fetch("https://ecommercetesting-70802-default-rtdb.europe-west1.firebasedatabase.app/products/-N__57-_D1ajNxQ4SEc3.json");
 
   if (!response.ok) {
     throw new Error(response.statusText);
